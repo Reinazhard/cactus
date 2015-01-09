6166d24c7bd690538ef2041c989ac0d6230dcc92
Minchan Kim <minchan@kernel.org>
2015-01-09 18:36:55 +0530

    mm: prevent to write out dirty page in CMA by may_writepage
    Now, local variable references in shrink_page_list is
    PAGEREF_RECLAIM_CLEAN as default. It is for preventing to reclaim
    dirty pages when CMA try to migrate pages.
    Strictly speaking, we don't need it because CMA already didn't allow
    to write out by .may_writepage = 0 in reclaim_clean_pages_from_list.
    
    Morever, it has a problem to prevent anonymous pages's swap out when
    we use force_reclaim = true in shrink_page_list(ex, per process reclaim
    can do it)
    
    So this patch makes references's default value to PAGEREF_RECLAIM
    and declare .may_writepage = 0 of scan_control in CMA part to make
    code more clear.
    
    Change-Id: I5edc3c955d106ecebc4949ce27daf5b7b7a18089
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Reported-by: Minkyung Kim <minkyung88@lge.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Patch-mainline: linux-mm @ 9 May 2013 16:21:23
    [vinmenon@codeaurora.org: trivial merge conflict fixes]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -963,7 +963,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 		struct address_space *mapping;
 		struct page *page;
 		int may_enter_fs;
-		enum page_references references = PAGEREF_RECLAIM_CLEAN;
+		enum page_references references = PAGEREF_RECLAIM;
 		bool dirty, writeback;
 		bool lazyfree = false;
 		int ret = SWAP_SUCCESS;
